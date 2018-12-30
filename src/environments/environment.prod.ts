1cc90537ecb09ac90634965be152248488c54683
Hugh Zabriskie <hugh.zabriskie@gmail.com>
2018-12-30 15:20:47 -0600

    update prod to hughzabriskieserver.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://35.166.193.79:443', 
+  apiUrl: 'https://hughzabriskieserver.com', 
   clientId: '1lkDlLmOvjZ1TbTpUO_fxQpe6FslEimq'
 };
